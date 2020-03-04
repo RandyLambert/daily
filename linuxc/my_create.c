6b7ca7d10b325a65b4cecb116fb3d7e8f8191ef7
RandyLambert <1249604462@qq.com>
2020-03-04 16:18:23 +0800

    hhhh

--- linuxc/my_create.c
+++ linuxc/my_create.c
@@ -7,14 +7,21 @@
 #include <stdlib.h>
 int main(){
     int fd;
-    if((fd = open("example_62.c",O_CREAT|O_EXCL,S_IRUSR |S_IWUSR)) == -1){
-        /* if((fd = creat("example_62.c",S_IRWXU)) == -1){ */
-            perror("open");
-            /* printf("open:%s    with errno:%d\n",strerror(errno),errno); */
-            exit(1);
-    }
-    else{
-            printf("creat accept\n");
+    /* if((fd = open("example_62.c",O_CREAT|O_EXCL,S_IRUSR |S_IWUSR)) == -1){ */
+    /*     /1* if((fd = creat("example_62.c",S_IRWXU)) == -1){ *1/ */
+    /*         perror("open"); */
+    /*         /1* printf("open:%s    with errno:%d\n",strerror(errno),errno); *1/ */
+    /*         exit(1); */
+    /* } */
+    /* else{ */
+    /*         printf("creat accept\n"); */
+    /* } */
+    /* close(fd); */
+
+
+    if((fd = open("test",O_WRONLY,0777)) == -1)
+    {
+        perror("errno open");
     }
     close(fd);
     return 0;

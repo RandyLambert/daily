d59ba112237ad3a4499ae15c389483e178400daa
RandyLambert <1249604462@qq.com>
2020-05-09 23:50:38 +0800

    hhhh

---
+++ c++/rand.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    for(int i = 0;i < 10;i++)
+    {
+        int x= rand()%100;    
+        cout<<x<<endl;
+    }
+
+
+    return 0;
+}
+

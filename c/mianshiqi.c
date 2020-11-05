284da88c54403d0557cea05107db4dd6de1474e5
RandyLambert <1249604462@qq.com>
2020-11-05 15:37:29 +0800

    hhhh

--- c/mianshiqi.c
+++ c/mianshiqi.c
@@ -3,24 +3,24 @@
 #include<string.h>
 int main(void)
 {
-    char Char[100];
+    char str[100];
     int key;
     char t;
-    fgets(Char,100,stdin);
+    fgets(str,100,stdin);
 
-    for(int i=0;i<strlen(Char)-1;i++){
+    for(int i=0;i<strlen(str)-1;i++){
         key=i;
-        for(int j=i+1;j<strlen(Char);j++){
-            if(Char[key]>Char[j]){
+        for(int j=i+1;j<strlen(str);j++){
+            if(str[key]>str[j]){
                 key=j;
             }
         } 
-        t=Char[key];
-        Char[key]=Char[i];
-        Char[i]=t;
+        t=str[key];
+        str[key]=str[i];
+        str[i]=t;
     }
 
-    puts(Char);
+    puts(str);
     return 0;
 }
 
